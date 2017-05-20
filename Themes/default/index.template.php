46757931309cce663965c390ba88469e8c5eefbc
Yami <yamikuronue@gmail.com>
2017-05-20 23:01:10 +0000

    created the link tree

--- Themes/default/index.template.php
+++ Themes/default/index.template.php
@@ -1,4 +1,6 @@
 <?php
+require(__DIR__ . '/../../vendor/autoload.php');
+use LightnCandy\LightnCandy;
 /**
  * Simple Machines Forum (SMF)
  *
@@ -76,100 +78,64 @@ function template_init()
 		$settings['disable_files'] = array();
 }
 
+function locale_helper($lang_locale) 
+{
+    return new \LightnCandy\SafeString(str_replace("_", "-", substr(lang_locale, 0, strcspn(lang_locale, "."))));
+}
+    
 /**
  * The main sub template above the content.
  */
 function template_html_above()
 {
 	global $context, $settings, $scripturl, $txt, $modSettings, $mbname;
-
-	// Show right to left, the language code, and the character set for ease of translating.
-	echo '<!DOCTYPE html>
-	<html', $context['right_to_left'] ? ' dir="rtl"' : '', !empty($txt['lang_locale']) ? ' lang="' . str_replace("_", "-", substr($txt['lang_locale'], 0, strcspn($txt['lang_locale'], "."))) . '"' : '', '>
-<head>
-	<meta charset="', $context['character_set'], '">';
-
-	// You don't need to manually load index.css, this will be set up for you. You can, of course, add
-	// any other files you want, after template_css() has been run. Note that RTL will also be loaded for you.
-
-	// The most efficient way of writing multi themes is to use a master index.css plus variant.css files.
-	// If you've set them up properly (through $settings['theme_variants'], loadCSSFile will load the variant files for you.
-
-	// load in any css from mods or themes so they can overwrite if wanted
-	template_css();
-
-	// load in any javascript files from mods and themes
-	template_javascript();
-
-	echo '
-	<title>', $context['page_title_html_safe'], '</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">';
-
-	// Content related meta tags, like description, keywords, Open Graph stuff, etc...
-	foreach ($context['meta_tags'] as $meta_tag)
-	{
-		echo '
-	<meta';
-
-		foreach ($meta_tag as $meta_key => $meta_value)
-			echo ' ', $meta_key, '="', $meta_value, '"';
-
-		echo '>';
+	
+	$data = Array(
+		'context' => $context,
+		'txt' => $txt,
+		'scripturl' => $scripturl,
+		'settings' => $settings
+	);
+	
+	$template = file_get_contents(__DIR__ .  "/layouts/html_above.hbs");
+	if (!$template) {
+		die('Template did not load!');
 	}
 
-	/* What is your Lollipop's color?
-	Theme Authors you can change here to make sure your theme's main color got visible on tab */
-	echo '
-	<meta name="theme-color" content="#557EA0">';
+	$phpStr = LightnCandy::compile($template, Array(
+	    'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_EXCEPTION | LightnCandy::FLAG_RENDER_DEBUG,
+	    'helpers' => Array(
+	        'locale' => 'locale_helper',
+	     /*   'css' => 'template_css',
+	        'js' => 'template_js'*/
+	    )
+	));
+	
+	//var_dump($context['meta_tags']);die();
+	$renderer = LightnCandy::prepare($phpStr);
+	echo $renderer($data);
+}
 
-	// Please don't index these Mr Robot.
-	if (!empty($context['robot_no_index']))
-		echo '
-	<meta name="robots" content="noindex">';
 
-	// Present a canonical url for search engines to prevent duplicate content in their indices.
-	if (!empty($context['canonical_url']))
-		echo '
-	<link rel="canonical" href="', $context['canonical_url'], '">';
+function login_helper($string, $guest_title, $forum_name, $scripturl, $login) 
+{
+    return new \LightnCandy\SafeString(sprintf($string,
+	    $guest_title, 
+	    $forum_name, 
+	    $scripturl . '?action=login', 
+	    'return reqOverlayDiv(this.href, ' . JavaScriptEscape($login) . ');', 
+	    $scripturl . '?action=signup'
+	));
+}
 
-	// Show all the relative links, such as help, search, contents, and the like.
-	echo '
-	<link rel="help" href="', $scripturl, '?action=help">
-	<link rel="contents" href="', $scripturl, '">', ($context['allow_search'] ? '
-	<link rel="search" href="' . $scripturl . '?action=search">' : '');
+function isSelected($current_val, $val) 
+{
+	
+    return new \LightnCandy\SafeString($current_val == $val ? 'selected="selected' : '');
+}
 
-	// If RSS feeds are enabled, advertise the presence of one.
-	if (!empty($modSettings['xmlnews_enable']) && (!empty($modSettings['allow_guestAccess']) || $context['user']['is_logged']))
-		echo '
-	<link rel="alternate feed" type="application/rss+xml" title="', $context['forum_name_html_safe'], ' - ', $txt['rss'], '" href="', $scripturl, '?action=.xml;type=rss2', !empty($context['current_board']) ? ';board=' . $context['current_board'] : '', '">
-	<link rel="alternate feed" type="application/atom+xml" title="', $context['forum_name_html_safe'], ' - ', $txt['atom'], '" href="', $scripturl, '?action=.xml;type=atom', !empty($context['current_board']) ? ';board=' . $context['current_board'] : '', '">';
-
-	// If we're viewing a topic, these should be the previous and next topics, respectively.
-	if (!empty($context['links']['next']))
-	{
-		echo '
-	<link rel="next" href="', $context['links']['next'], '">';
-	}
-
-	if (!empty($context['links']['prev']))
-	{
-		echo '
-	<link rel="prev" href="', $context['links']['prev'], '">';
-	}
-
-	// If we're in a board, or a topic for that matter, the index will be the board's index.
-	if (!empty($context['current_board']))
-		echo '
-	<link rel="index" href="', $scripturl, '?board=', $context['current_board'], '.0">';
-
-	// Output any remaining HTML headers. (from mods, maybe?)
-	echo $context['html_headers'];
-
-	echo '
-</head>
-<body id="', $context['browser_body_id'], '" class="action_', !empty($context['current_action']) ? $context['current_action'] : (!empty($context['current_board']) ?
-		'messageindex' : (!empty($context['current_topic']) ? 'display' : 'home')), !empty($context['current_board']) ? ' board_' . $context['current_board'] : '', '">
-<div id="footerfix">';
+function langName($lang) {
+	return  str_replace('-utf8', '', $language['name']);
 }
 
 /**
@@ -179,185 +145,36 @@ function template_body_above()
 {
 	global $context, $settings, $scripturl, $txt, $modSettings, $maintenance;
 
-	// Wrapper div now echoes permanently for better layout options. h1 a is now target for "Go up" links.
-	echo '
-	<div id="top_section">';
-
-	// If the user is logged in, display some things that might be useful.
-	if ($context['user']['is_logged'])
-	{
-		// Firstly, the user's menu
-		echo '
-		<ul class="floatleft" id="top_info">
-			<li>
-				<a href="', $scripturl, '?action=profile"', !empty($context['self_profile']) ? ' class="active"' : '', ' id="profile_menu_top" onclick="return false;">';
-					if (!empty($context['user']['avatar']))
-						echo $context['user']['avatar']['image'];
-					echo $context['user']['name'], '</a>
-				<div id="profile_menu" class="top_menu"></div>
-			</li>';
-
-		// Secondly, PMs if we're doing them
-		if ($context['allow_pm'])
-		{
-			echo '
-			<li>
-				<a href="', $scripturl, '?action=pm"', !empty($context['self_pm']) ? ' class="active"' : '', ' id="pm_menu_top">', $txt['pm_short'], !empty($context['user']['unread_messages']) ? ' <span class="amt">' . $context['user']['unread_messages'] . '</span>' : '', '</a>
-				<div id="pm_menu" class="top_menu scrollable"></div>
-			</li>';
-		}
-
-		// Thirdly, alerts
-		echo '
-			<li>
-				<a href="', $scripturl, '?action=profile;area=showalerts;u=', $context['user']['id'], '"', !empty($context['self_alerts']) ? ' class="active"' : '', ' id="alerts_menu_top">', $txt['alerts'], !empty($context['user']['alerts']) ? ' <span class="amt">' . $context['user']['alerts'] . '</span>' : '', '</a>
-				<div id="alerts_menu" class="top_menu scrollable"></div>
-			</li>';
-
-		// And now we're done.
-		echo '
-		</ul>';
-	}
-	// Otherwise they're a guest. Ask them to either register or login.
-	else
-		if (empty($maintenance))
-			echo '
-			<ul class="floatleft welcome">
-				<li>', sprintf($txt[$context['can_register'] ? 'welcome_guest_register' : 'welcome_guest'], $txt['guest_title'], $context['forum_name_html_safe'], $scripturl . '?action=login', 'return reqOverlayDiv(this.href, ' . JavaScriptEscape($txt['login']) . ');', $scripturl . '?action=signup'), '</li>
-			</ul>';
-		else
-			//In maintenance mode, only login is allowed and don't show OverlayDiv
-			echo '
-			<ul class="floatleft welcome">
-				<li>', sprintf($txt['welcome_guest'], $txt['guest_title'], '', $scripturl. '?action=login', 'return true;'), '</li>
-			</ul>';
-
-	if (!empty($modSettings['userLanguage']) && !empty($context['languages']) && count($context['languages']) > 1)
-	{
-		echo '
-		<form id="languages_form" method="get" class="floatright">
-			<select id="language_select" name="language" onchange="this.form.submit()">';
-
-		foreach ($context['languages'] as $language)
-			echo '
-				<option value="', $language['filename'], '"', isset($context['user']['language']) && $context['user']['language'] == $language['filename'] ? ' selected="selected"' : '', '>', str_replace('-utf8', '', $language['name']), '</option>';
-
-		echo '
-			</select>
-			<noscript>
-				<input type="submit" value="', $txt['quick_mod_go'], '" />
-			</noscript>
-		</form>';
+	$data = Array(
+		'context' => $context,
+		'txt' => $txt,
+		'scripturl' => $scripturl,
+		'settings' => $settings,
+		'maintenance' => $maintenance,
+		'modSettings' => $modSettings,
+		'menu' => template_menu()
+	);
+	
+	$template = file_get_contents(__DIR__ .  "/layouts/body_above.hbs");
+	if (!$template) {
+		die('Template did not load!');
 	}
 
-	if ($context['allow_search'])
-	{
-		echo '
-		<form id="search_form" class="floatright" action="', $scripturl, '?action=search2" method="post" accept-charset="', $context['character_set'], '">
-			<input type="search" name="search" value="" class="input_text">&nbsp;';
+	$phpStr = LightnCandy::compile($template, Array(
+	    'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_EXCEPTION | LightnCandy::FLAG_RENDER_DEBUG,
+	    'helpers' => Array(
+	        'login_helper' => 'login_helper',
+	        'isSelected',
+	        'langName'
+	     /*   'css' => 'template_css',
+	        'js' => 'template_js'*/
+	    )
+	));
+	
+	//var_dump($context['meta_tags']);die();
+	$renderer = LightnCandy::prepare($phpStr);
+	echo $renderer($data);
 
-		// Using the quick search dropdown?
-		$selected = !empty($context['current_topic']) ? 'current_topic' : (!empty($context['current_board']) ? 'current_board' : 'all');
-
-		echo '
-			<select name="search_selection">
-				<option value="all"', ($selected == 'all' ? ' selected' : ''), '>', $txt['search_entireforum'], ' </option>';
-
-		// Can't limit it to a specific topic if we are not in one
-		if (!empty($context['current_topic']))
-			echo '
-				<option value="topic"', ($selected == 'current_topic' ? ' selected' : ''), '>', $txt['search_thistopic'], '</option>';
-
-		// Can't limit it to a specific board if we are not in one
-		if (!empty($context['current_board']))
-			echo '
-				<option value="board"', ($selected == 'current_board' ? ' selected' : ''), '>', $txt['search_thisbrd'], '</option>';
-
-		// Can't search for members if we can't see the memberlist
-		if (!empty($context['allow_memberlist']))
-			echo '
-				<option value="members"', ($selected == 'members' ? ' selected' : ''), '>', $txt['search_members'], ' </option>';
-
-		echo '
-			</select>';
-
-		// Search within current topic?
-		if (!empty($context['current_topic']))
-			echo '
-			<input type="hidden" name="sd_topic" value="', $context['current_topic'], '">';
-		// If we're on a certain board, limit it to this board ;).
-		elseif (!empty($context['current_board']))
-			echo '
-			<input type="hidden" name="sd_brd" value="', $context['current_board'], '">';
-
-		echo '
-			<input type="submit" name="search2" value="', $txt['search'], '" class="button_submit">
-			<input type="hidden" name="advanced" value="0">
-		</form>';
-	}
-
-	echo '
-	</div>';
-
-	echo '
-	<div id="header">
-		<h1 class="forumtitle">
-			<a id="top" href="', $scripturl, '">', empty($context['header_logo_url_html_safe']) ? $context['forum_name_html_safe'] : '<img src="' . $context['header_logo_url_html_safe'] . '" alt="' . $context['forum_name_html_safe'] . '">', '</a>
-		</h1>';
-
-	echo '
-		', empty($settings['site_slogan']) ? '<img id="smflogo" src="' . $settings['images_url'] . '/smflogo.png" alt="Simple Machines Forum" title="Simple Machines Forum">' : '<div id="siteslogan" class="floatright">' . $settings['site_slogan'] . '</div>', '';
-
-	echo'
-	</div>
-	<div id="wrapper">
-		<div id="upper_section">
-			<div id="inner_section">
-				<div id="inner_wrap">
-					<div class="user">
-						', $context['current_time'], '
-					</div>';
-	// Show a random news item? (or you could pick one from news_lines...)
-	if (!empty($settings['enable_news']) && !empty($context['random_news_line']))
-		echo '
-					<div class="news">
-						<h2>', $txt['news'], ': </h2>
-						<p>', $context['random_news_line'], '</p>
-					</div>';
-
-	echo '
-					<hr class="clear">
-				</div>';
-
-	// Load mobile menu here
-	echo '
-				<a class="menu_icon mobile_user_menu"></a>
-				<div id="mobile_user_menu" class="popup_container">
-					<div class="popup_window description">
-						<div class="popup_heading">', $txt['mobile_user_menu'], '
-						<a href="javascript:void(0);" class="generic_icons hide_popup"></a></div>
-						', template_menu(), '
-					</div>
-				</div>';
-
-	// Show the menu here, according to the menu sub template, followed by the navigation tree.
-	echo '
-	<div id="main_menu">';
-		template_menu();
-
-	echo '
-	</div>';
-
-	theme_linktree();
-
-	echo '
-			</div>
-		</div>';
-
-	// The main content should go here.
-	echo '
-		<div id="content_section">
-			<div id="main_content_section">';
 }
 
 /**
@@ -366,33 +183,31 @@ function template_body_above()
 function template_body_below()
 {
 	global $context, $txt, $scripturl, $modSettings;
+	
+	$data = Array(
+		'context' => $context,
+		'txt' => $txt,
+		'scripturl' => $scripturl,
+		'settings' => $settings,
+		'modSettings' => $modSettings,
+		'copyright' => theme_copyright(),
+		'loadtime' => sprintf($txt['page_created_full'], $context['load_time'], $context['load_queries'])
+	);
+	
+	$template = file_get_contents(__DIR__ .  "/layouts/body_below.hbs");
+	if (!$template) {
+		die('Template did not load!');
+	}
 
-	echo '
-			</div>
-		</div>
-	</div>
-</div>';
-
-	// Show the XHTML, RSS and WAP2 links, as well as the copyright.
-	// Footer is now full-width by default.
-	echo '
-	<div id="footer">';
-
-	// There is now a global "Go to top" link at the right.
-		echo '
-		<ul>
-			<li class="floatright"><a href="', $scripturl, '?action=help">', $txt['help'], '</a> ', (!empty($modSettings['requireAgreement'])) ? '| <a href="' . $scripturl . '?action=help;sa=rules">' . $txt['terms_and_rules'] . '</a>' : '', ' | <a href="#top_section">', $txt['go_up'], ' &#9650;</a></li>
-			<li class="copyright">', theme_copyright(), '</li>
-		</ul>';
-
-	// Show the load time?
-	if ($context['show_load_time'])
-		echo '
-		<p>', sprintf($txt['page_created_full'], $context['load_time'], $context['load_queries']), '</p>';
-
-	echo '
-	</div>';
-
+	$phpStr = LightnCandy::compile($template, Array(
+	    'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_EXCEPTION | LightnCandy::FLAG_RENDER_DEBUG,
+	    'helpers' => Array(
+	    )
+	));
+	
+	//var_dump($context['meta_tags']);die();
+	$renderer = LightnCandy::prepare($phpStr);
+	echo $renderer($data);
 }
 
 /**
@@ -400,76 +215,9 @@ function template_body_below()
  */
 function template_html_below()
 {
-	// load in any javascipt that could be deferred to the end of the page
-	template_javascript(true);
-
 	echo '
 </body>
 </html>';
-}
-
-/**
- * Show a linktree. This is that thing that shows "My Community | General Category | General Discussion"..
- *
- * @param bool $force_show Whether to force showing it even if settings say otherwise
- */
-function theme_linktree($force_show = false)
-{
-	global $context, $shown_linktree, $scripturl, $txt;
-
-	// If linktree is empty, just return - also allow an override.
-	if (empty($context['linktree']) || (!empty($context['dont_default_linktree']) && !$force_show))
-		return;
-
-	echo '
-				<div class="navigate_section">
-					<ul>';
-
-	if ($context['user']['is_logged'])
-	echo '
-						<li class="unread_links">
-							<a href="', $scripturl, '?action=unread" title="', $txt['unread_since_visit'], '">', $txt['view_unread_category'], '</a>
-							<a href="', $scripturl, '?action=unreadreplies" title="', $txt['show_unread_replies'], '">', $txt['unread_replies'], '</a>
-						</li>';
-
-	// Each tree item has a URL and name. Some may have extra_before and extra_after.
-	foreach ($context['linktree'] as $link_num => $tree)
-	{
-		echo '
-						<li', ($link_num == count($context['linktree']) - 1) ? ' class="last"' : '', '>';
-
-		// Don't show a separator for the first one.
-		// Better here. Always points to the next level when the linktree breaks to a second line.
-		// Picked a better looking HTML entity, and added support for RTL plus a span for styling.
-		if ($link_num != 0)
-			echo '
-							<span class="dividers">', $context['right_to_left'] ? ' &#9668; ' : ' &#9658; ', '</span>';
-
-		// Show something before the link?
-		if (isset($tree['extra_before']))
-			echo $tree['extra_before'], ' ';
-
-		// Show the link, including a URL if it should have one.
-		if (isset($tree['url']))
-			echo '
-					<a href="' . $tree['url'] . '"><span>' . $tree['name'] . '</span></a>';
-		else
-			echo '
-					<span>' . $tree['name'] . '</span>';
-
-		// Show something after the link...?
-		if (isset($tree['extra_after']))
-			echo ' ', $tree['extra_after'];
-
-		echo '
-						</li>';
-	}
-
-	echo '
-					</ul>
-				</div>';
-
-	$shown_linktree = true;
 }
 
 /**
