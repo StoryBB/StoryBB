d971396e14ffec700cf52cfa846a9645bbce93bb
Yami <yamikuronue@gmail.com>
2017-08-14 22:57:09 +0000

    feature: quick reply and quote fast

--- Themes/default/Post.template.php
+++ Themes/default/Post.template.php
@@ -81,51 +81,27 @@ function template_quotefast()
 {
 	global $context, $settings, $txt, $modSettings;
 
-	echo '<!DOCTYPE html>
-<html', $context['right_to_left'] ? ' dir="rtl"' : '', '>
-	<head>
-		<meta charset="UTF-8">
-		<title>', $txt['retrieving_quote'], '</title>
-		<script src="', $settings['default_theme_url'], '/scripts/script.js', $modSettings['browser_cache'] ,'"></script>
-	</head>
-	<body>
-		', $txt['retrieving_quote'], '
-		<div id="temporary_posting_area" style="display: none;"></div>
-		<script>';
+		$data = [
+		'context' => $context,
+		'txt' => $txt,
+		'modSettings' => $modSettings,
+		'settings' => $settings
+	];
 
-	if ($context['close_window'])
-		echo '
-			window.close();';
-	else
-	{
-		// Lucky for us, Internet Explorer has an "innerText" feature which basically converts entities <--> text. Use it if possible ;).
-		echo '
-			var quote = \'', $context['quote']['text'], '\';
-			var stage = \'createElement\' in document ? document.createElement("DIV") : document.getElementById("temporary_posting_area");
-
-			if (\'DOMParser\' in window && !(\'opera\' in window))
-			{
-				var xmldoc = new DOMParser().parseFromString("<temp>" + \'', $context['quote']['mozilla'], '\'.replace(/\n/g, "_SMF-BREAK_").replace(/\t/g, "_SMF-TAB_") + "</temp>", "text/xml");
-				quote = xmldoc.childNodes[0].textContent.replace(/_SMF-BREAK_/g, "\n").replace(/_SMF-TAB_/g, "\t");
-			}
-			else if (\'innerText\' in stage)
-			{
-				setInnerHTML(stage, quote.replace(/\n/g, "_SMF-BREAK_").replace(/\t/g, "_SMF-TAB_").replace(/</g, "&lt;").replace(/>/g, "&gt;"));
-				quote = stage.innerText.replace(/_SMF-BREAK_/g, "\n").replace(/_SMF-TAB_/g, "\t");
-			}
-
-			if (\'opera\' in window)
-				quote = quote.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&quot;/g, \'"\').replace(/&amp;/g, "&");
-
-			window.opener.onReceiveOpener(quote);
-
-			window.focus();
-			setTimeout("window.close();", 400);';
+	$template = file_get_contents(__DIR__ .  "/templates/post_quotefast.hbs");
+	if (!$template) {
+		die('Display main template did not load!');
 	}
-	echo '
-		</script>
-	</body>
-</html>';
+
+	$phpStr = LightnCandy::compile($template, [
+		'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_EXCEPTION | LightnCandy::FLAG_RENDER_DEBUG | LightnCandy::FLAG_RUNTIMEPARTIAL,
+		'helpers' => [
+		],
+		'partials' => [
+		]
+	]);
+	$renderer = LightnCandy::prepare($phpStr);
+	return $renderer($data);
 }
 
 /**
