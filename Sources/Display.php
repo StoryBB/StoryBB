f47405dbdf95b2483f2ef9d443380a822c47e660
Arantor <sleeping@myperch.org>
2017-10-19 23:14:45 +0100

    Move the helper stuff that's still outstanding into the controller (and rework it slightly for scope issues)

--- Sources/Display.php
+++ Sources/Display.php
@@ -1248,6 +1248,41 @@ function Display()
 		loadJavaScriptFile('jquery.caret.min.js', array('defer' => true), 'smf_caret');
 		loadJavaScriptFile('mentions.js', array('defer' => true), 'smf_mentions');
 	}
+
+	// Some convenient template setup.
+	$context['sub_template'] = 'display_main';
+	register_helper([
+		'getLikeText' => function($likes) {
+			global $txt, $context;
+			
+			$base = 'likes_';
+			$count = $likes['count'];
+			if ($likes['you'])
+			{
+				$base = 'you_' . $base;
+				$count--;
+			}
+			$base .= (isset($txt[$base . $count])) ? $count : 'n';
+			return sprintf($txt[$base], $scripturl . '?action=likes;sa=view;ltype=msg;like=' . $id . ';' . $context['session_var'] . '=' . $context['session_id'], comma_format($count));
+		}
+	]);
+
+	$context['viewing'] = '';
+	if (!empty($settings['display_who_viewing']))
+	{
+		$context['viewing'] = $settings['display_who_viewing'] == 1 ? 
+				count($context['view_members']) . ' ' . count($context['view_members']) == 1 ? $txt['who_member'] : $txt['members']
+			:
+				empty($context['view_members_list']) ? '0 ' . $txt['members'] : implode(', ', $context['view_members_list']) . ((empty($context['view_num_hidden']) || $context['can_moderate_forum']) ? '' : ' (+ ' . $context['view_num_hidden'] . ' ' . $txt['hidden'] . ')');
+	}
+	$context['messages'] = [];
+	$context['ignoredMsgs'] = [];
+	$context['removableMessageIDs'] = [];
+	while($message = $context['get_message']()) {
+		$context['messages'][] = $message;
+		if (!empty($message['is_ignored'])) $context['ignoredMsgs'][] = $message['id'];
+		if ($message['can_remove']) $context['removableMessageIDs'][] = $message['id'];
+	}
 }
 
 /**
