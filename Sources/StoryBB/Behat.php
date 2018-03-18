45a7ff13ced57bff0368df5b86e7295198c8fcb4
Arantor <sleeping@myperch.org>
2018-03-18 14:11:38 +0000

    Fix licence details.

--- Sources/StoryBB/Behat.php
+++ Sources/StoryBB/Behat.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Manage and maintain the boards and categories of the forum.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 3.0 Alpha 1
+ */
+
 namespace StoryBB;
 
 use Behat\Behat\Context\Context;
