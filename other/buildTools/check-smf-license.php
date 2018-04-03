d3038b6bb32d9b89c333501057d57f68526a9b4a
Arantor <sleeping@myperch.org>
2018-04-03 21:07:08 +0100

    Add a docblock to the Smileys index files (and remove them from the exclusion list for the licence check)

--- other/buildTools/check-smf-license.php
+++ other/buildTools/check-smf-license.php
@@ -14,8 +14,6 @@ $ignoreFiles = array(
 	'\./attachments/index\.php',
 	'\./cache/index\.php',
 	'\./custom_avatar/index\.php',
-	'\./Smileys/[A-Za-z0-9]+/index\.php',
-	'\./Smileys/index\.php',
 	'\./Sources/index\.php',
 	'\./Sources/tasks/index\.php',
 	'\./Themes/default/css/index\.php',
