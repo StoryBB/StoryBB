d8ee2a652400830000e924a6dde91acee083ebce
Arantor <sleeping@myperch.org>
2018-04-08 20:07:27 +0100

    Rearrange the modifications page when empty to push content to a full-width row (and do it properly; didn't work properly before)

--- Sources/ManageSettings.php
+++ Sources/ManageSettings.php
@@ -2147,7 +2147,7 @@ function ModifyGeneralModSettings($return_config = false)
 	if (empty($config_vars))
 	{
 		$context['settings_save_dont_show'] = true;
-		$context['settings_message'] = '<div class="centertext">' . $txt['modification_no_misc_settings'] . '</div>';
+		$config_vars[] = $txt['modification_no_misc_settings'];
 
 		return prepareDBSettingContext($config_vars);
 	}
