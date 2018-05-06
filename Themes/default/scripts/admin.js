bcc5e2e9ebd3895d0b15d388b7f91a940a973491
Arantor <sleeping@myperch.org>
2018-05-06 23:40:54 +0100

    Replace out the home page JavaScript to use the JSON data in local context.

--- Themes/default/scripts/admin.js
+++ Themes/default/scripts/admin.js
@@ -1,13 +1,4 @@
 /*
-	smf_AdminIndex(oOptions)
-	{
-		public init()
-		public loadAdminIndex()
-		public setAnnouncements()
-		public showCurrentVersion()
-		public checkUpdateAvailable()
-	}
-
 	smf_ViewVersions(oOptions)
 	{
 		public init()
@@ -17,94 +8,6 @@
 		public determineVersions()
 	}
 */
-
-
-
-// Handle the JavaScript surrounding the admin and moderation center.
-function smf_AdminIndex(oOptions)
-{
-	var defaults = {
-		bLoadAnnouncements: true,
-		bLoadVersions: true,
-		bLoadUpdateNotification: true,
-		sVersionOutdatedTemplate: '<span class="alert">%currentVersion%</span>'
-	};
-	this.opt = $.extend({}, defaults, oOptions || {});
-	this.init();
-}
-
-smf_AdminIndex.prototype.init = function ()
-{
-	window.adminIndexInstanceRef = this;
-	var fHandlePageLoaded = function () {
-		window.adminIndexInstanceRef.loadAdminIndex();
-	}
-	addLoadEvent(fHandlePageLoaded);
-}
-
-smf_AdminIndex.prototype.loadAdminIndex = function ()
-{
-	// Load the text box containing the latest news items.
-	if (this.opt.bLoadAnnouncements)
-		this.setAnnouncements();
-
-	// Load the current StoryBB and your StoryBB version numbers.
-	if (this.opt.bLoadVersions)
-		this.showCurrentVersion();
-
-	// Load the text box that sais there's a new version available.
-	if (this.opt.bLoadUpdateNotification)
-		this.checkUpdateAvailable();
-}
-
-
-smf_AdminIndex.prototype.setAnnouncements = function ()
-{
-	if (!('smfAnnouncements' in window) || !('length' in window.smfAnnouncements))
-		return;
-
-	var sMessages = '';
-	for (var i = 0; i < window.smfAnnouncements.length; i++)
-		sMessages += this.opt.sAnnouncementMessageTemplate.replace('%href%', window.smfAnnouncements[i].href).replace('%subject%', window.smfAnnouncements[i].subject).replace('%time%', window.smfAnnouncements[i].time).replace('%message%', window.smfAnnouncements[i].message);
-
-	setInnerHTML(document.getElementById(this.opt.sAnnouncementContainerId), this.opt.sAnnouncementTemplate.replace('%content%', sMessages));
-}
-
-smf_AdminIndex.prototype.showCurrentVersion = function ()
-{
-	if (!('smfVersion' in window))
-		return;
-
-	var oSmfVersionContainer = document.getElementById(this.opt.sSmfVersionContainerId);
-	var oYourVersionContainer = document.getElementById(this.opt.sYourVersionContainerId);
-
-	setInnerHTML(oSmfVersionContainer, window.smfVersion);
-
-	var sCurrentVersion = getInnerHTML(oYourVersionContainer);
-	if (sCurrentVersion != window.smfVersion)
-		setInnerHTML(oYourVersionContainer, this.opt.sVersionOutdatedTemplate.replace('%currentVersion%', sCurrentVersion));
-}
-
-smf_AdminIndex.prototype.checkUpdateAvailable = function ()
-{
-	if (!('smfUpdatePackage' in window))
-		return;
-
-	var oContainer = document.getElementById(this.opt.sUpdateNotificationContainerId);
-
-	// Are we setting a custom title and message?
-	var sTitle = 'smfUpdateTitle' in window ? window.smfUpdateTitle : this.opt.sUpdateNotificationDefaultTitle;
-	var sMessage = 'smfUpdateNotice' in window ? window.smfUpdateNotice : this.opt.sUpdateNotificationDefaultMessage;
-
-	setInnerHTML(oContainer, this.opt.sUpdateNotificationTemplate.replace('%title%', sTitle).replace('%message%', sMessage));
-
-	// Parse in the package download URL if it exists in the string.
-	document.getElementById('update-link').href = this.opt.sUpdateNotificationLink.replace('%package%', window.smfUpdatePackage);
-
-	oContainer.className = ('smfUpdateCritical' in window) ? 'errorbox' : 'noticebox';
-}
-
-
 
 function smf_ViewVersions (oOptions)
 {
