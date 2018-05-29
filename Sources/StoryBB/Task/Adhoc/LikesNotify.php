2e89f3423667fa6b527288529dfea51ad5c2470c
Arantor <sleeping@myperch.org>
2018-05-29 20:08:01 +0100

    Convert another.

--- Sources/StoryBB/Task/Adhoc/LikesNotify.php
+++ Sources/StoryBB/Task/Adhoc/LikesNotify.php
@@ -10,10 +10,12 @@
  * @version 3.0 Alpha 1
  */
 
+namespace StoryBB\Task\Adhoc;
+
 /**
  * Class Likes_Notify_Background
  */
-class Likes_Notify_Background extends SMF_BackgroundTask
+class LikesNotify extends StoryBB\Task\Adhoc
 {
 	/**
 	 * This executes the task - loads up the information, puts the email in the queue and inserts alerts as needed.
