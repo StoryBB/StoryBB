33572be63539897da6ee7b553f30822e63b2e734
Arantor <sleeping@myperch.org>
2018-06-12 23:18:58 +0100

    Fix a couple of oddities in user help.

--- Sources/Help.php
+++ Sources/Help.php
@@ -58,7 +58,7 @@ function HelpIndex()
 	);
 
 	// Lastly, some minor template stuff.
-	$context['page_title'] = $txt['manual_smf_user_help'];
+	$context['page_title'] = $txt['manual_storybb_user_help'];
 	$context['sub_template'] = 'help_manual';
 }
 
--- Themes/default/languages/Manual.english.php
+++ Themes/default/languages/Manual.english.php
@@ -5,7 +5,7 @@
    If you are looking at translating the manual into another language
    please visit the Simple Machines website for tools to assist! */
 
-$txt['manual_smf_user_help'] = 'StoryBB User Help';
+$txt['manual_storybb_user_help'] = 'StoryBB User Help';
 
 $txt['manual_terms_and_rules'] = 'These are the site terms and rules you must agree to before using the site.';
 
