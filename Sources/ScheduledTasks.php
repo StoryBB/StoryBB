96a0ed3e9574471345e014b4aad2457c2952a0a3
Arantor <sleeping@myperch.org>
2018-07-21 18:21:42 +0100

    Log scrubber scheduled task.

--- Sources/ScheduledTasks.php
+++ Sources/ScheduledTasks.php
@@ -852,13 +852,20 @@ function scheduled_clean_exports()
 function scheduled_scrub_logs()
 {
 	global $modSettings, $smcFunc;
+	$now = time();
+
+	$standard_period = isset($modSettings['retention_policy_standard']) ? (int) $modSettings['retention_policy_standard'] : 90;
+	$standard_timestamp = $now - ($standard_period * 86400);
+
+	$sensitive_period = isset($modSettings['retention_policy_sensitive']) ? (int) $modSettings['retention_policy_sensitive'] : 15;
+	$sensitive_timestamp = $now - ($sensitive_period * 86400);
 
 	// The flood control table keeps IPs - so we have to clean it. Anything more than a day old shouldn't exist anyway...
 	$smcFunc['db_query']('', '
 		DELETE FROM {db_prefix}log_floodcontrol
 		WHERE log_time < {int:log_time}',
 		[
-			'log_time' => time() - 86400,
+			'log_time' => $now - 86400,
 		]
 	);
 
@@ -867,11 +874,100 @@ function scheduled_scrub_logs()
 		DELETE FROM {db_prefix}log_online
 		WHERE log_time < {int:log_time}',
 		[
-			'log_time' => time() - 86400 - 3600,
+			'log_time' => $now - 86400 - 3600,
 		]
 	);
 
+	// Now fix the error log, first session IDs.
+	$smcFunc['db_query']('', '
+		UPDATE {db_prefix}log_errors
+		SET session = {empty}
+		WHERE log_time < {int:log_time}',
+		[
+			'log_time' => $sensitive_timestamp, 
+		]
+	);
+
+	// Now we go to town and cleaning up IPs in various places.
+	erase_log_table('log_actions', 'id_action', 'ip', 'log_time', $standard_timestamp);
+	erase_log_table('log_banned', 'id_ban_log', 'ip', 'log_time', $standard_timestamp);
+	erase_log_table('log_errors', 'id_error', 'ip', 'log_time', $sensitive_timestamp);
+	erase_log_table('log_reported_comments', 'id_comment', 'member_ip', 'time_sent', $standard_timestamp);
+	erase_log_table('members', 'id_member', 'member_ip', 'last_login', $standard_timestamp);
+	erase_log_table('members', 'id_member', 'member_ip2', 'last_login', $standard_timestamp);
+	erase_log_table('member_logins', 'id_login', 'ip', 'time', $standard_timestamp);
+	erase_log_table('member_logins', 'id_login', 'ip2', 'time', $standard_timestamp);
+	erase_log_table('messages', 'id_msg', 'poster_ip', 'poster_time', $standard_timestamp);
+
 	return true;
+}
+
+function erase_log_table($table_name, $id_column, $ip_column, $time_column, $timestamp)
+{
+	global $smcFunc;
+	static $banned_ips = null;
+
+	if ($banned_ips === null)
+	{
+		// Before we go any further, we need the banned IPs so that we can exclude them going forward.
+		$banned_ips = [];
+		$request = $smcFunc['db_query']('', '
+			SELECT ip_low, ip_high
+			FROM {db_prefix}ban_items AS bi
+				INNER JOIN {db_prefix}ban_groups AS bg ON (bi.id_ban_group = bg.id_ban_group)
+			WHERE bg.expire_time IS NULL or bg.expire_time > {int:now}',
+			[
+				'now' => time(),
+			]
+		);
+		while ($row = $smcFunc['db_fetch_assoc']($request))
+		{
+			$banned_ips[] = $row;
+		}
+		$smcFunc['db_free_result']($request);
+	}
+
+	// Find any rows that might be relevant, check they're not banned, and add them to the list.
+	$erase = [];
+	$request = $smcFunc['db_query']('', '
+		SELECT {raw:id_column}, {raw:ip_column}
+		FROM {db_prefix}' . $table_name . '
+		WHERE {raw:time_column} < {int:log_time}',
+		[
+			'id_column' => $id_column,
+			'ip_column' => $ip_column,
+			'time_column' => $time_column,
+			'log_time' => $timestamp, 
+		]
+	);
+	while ($row = $smcFunc['db_fetch_assoc']($request))
+	{
+		foreach ($banned_ips as $ban)
+		{
+			if ($row[$ip_column] >= $ban['ip_low'] && $row[$ip_column] <= $ban['ip_high'])
+			{
+				continue 2;
+			}
+		}
+
+		$erase[] = $row[$id_column];
+	}
+	$smcFunc['db_free_result']($request);
+
+	// Now the actual deletion.
+	if (empty($erase))
+		return;
+
+	$smcFunc['db_query']('', '
+		UPDATE {db_prefix}' . $table_name . '
+		SET {raw:ip_column} = NULL
+		WHERE {raw:id_column} IN ({array_int:erase})',
+		[
+			'id_column' => $id_column,
+			'ip_column' => $ip_column,
+			'erase' => $erase,
+		]
+	);
 }
 
 /**
