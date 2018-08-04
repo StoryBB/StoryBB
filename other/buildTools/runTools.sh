0a81ffd39b200897d33d170405592bcbf5fdb1c3
Arantor <sleeping@myperch.org>
2018-08-04 15:31:39 +0100

    Travis doesn't run this script for doing anything.

--- other/buildTools/runTools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run all the scripts.
-if find . -name "*.php" ! -path "./vendor/*" -exec php -l {} 2>&1 \; | grep "syntax error, unexpected"; then exit 1; fi
-
-if find . -name "*.php" ! -path "./vendor/*" -exec php other/buildTools/check-sbb-license.php {} 2>&1 \; | grep "Error:"; then exit 1; fi
