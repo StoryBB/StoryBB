67675a519f59ca984dace00428e5a3eca2c15aa2
Arantor <sleeping@myperch.org>
2018-08-04 13:52:31 +0100

    Add basic Redis support for caching.

--- Sources/ManageServer.php
+++ Sources/ManageServer.php
@@ -984,7 +984,7 @@ function saveSettings(&$config_vars)
 		'webmaster_email',
 		'db_name', 'db_user', 'db_server', 'db_prefix', 'ssi_db_user',
 		'boarddir', 'sourcedir',
-		'cachedir', 'cachedir_sqlite', 'cache_accelerator', 'cache_memcached',
+		'cachedir', 'cachedir_sqlite', 'cache_accelerator', 'cache_memcached', 'cache_redis',
 		'image_proxy_secret',
 	);
 
