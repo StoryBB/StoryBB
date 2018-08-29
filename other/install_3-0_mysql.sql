9dabfe516825a730ac23b910b811f0d406261871
Arantor <sleeping@myperch.org>
2018-08-29 23:48:28 +0100

    Move daily maintenance to its own class.

--- other/install_3-0_mysql.sql
+++ other/install_3-0_mysql.sql
@@ -1767,7 +1767,7 @@ INSERT INTO {$db_prefix}scheduled_tasks
   (id_task, next_time, time_offset, time_regularity, time_unit, disabled, task, class)
 VALUES
   (1, 0, 0, 2, 'h', 0, 'approval_notification', ''),
-  (3, 0, 60, 1, 'd', 0, 'daily_maintenance', ''),
+  (3, 0, 60, 1, 'd', 0, 'daily_maintenance', 'StoryBB\\Task\\Schedulable\\DailyMaintenance'),
   (5, 0, 0, 1, 'd', 0, 'daily_digest', ''),
   (6, 0, 0, 1, 'w', 0, 'weekly_digest', ''),
   (7, 0, {$sched_task_offset}, 1, 'd', 0, 'fetchStoryBBfiles', ''),
