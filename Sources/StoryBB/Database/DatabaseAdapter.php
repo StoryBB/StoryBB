2a0d90cc4d35796a4c76ef27c9cdb1cf864f0631
Arantor <sleeping@myperch.org>
2019-01-26 14:28:41 +0000

    Starting to migrate DB adapter logic to classes with a factory.

---
+++ Sources/StoryBB/Database/DatabaseAdapter.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Any database connector should implement this.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 3.0 Alpha 1
+ */
+
+namespace StoryBB\Database;
+
+interface DatabaseAdapter
+{
+
+}
---
+++ Sources/StoryBB/Database/Exception/ConnectionFailedException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Exceptions from within the database system.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 3.0 Alpha 1
+ */
+
+namespace StoryBB\Database\Exception;
+
+/**
+ * Exceptions from within the database system.
+ */
+class ConnectionFailedException extends \StoryBB\Database\Exception
+{
+}
