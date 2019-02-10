ecada26c8e4180fceb5ead1c0243c59a8bcdea19
Arantor <sleeping@myperch.org>
2019-02-10 17:59:19 +0000

    Convert remove topic redirects also to being its own class.

---
+++ Sources/StoryBB/Task/Schedulable/RemoveTopicRedirects.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Check for and remove move topic notices that have expired.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 3.0 Alpha 1
+ */
+
+namespace StoryBB\Task\Schedulable;
+
+/**
+ * Check for and remove move topic notices that have expired.
+ */
+class RemoveTopicRedirects extends \StoryBB\Task\Schedulable
+{
+	/**
+	 * Check for and remove move topic notices that have expired.
+	 * @return bool True on success
+	 */
+	public function execute(): bool
+	{
+		global $smcFunc, $sourcedir;
+
+		// init
+		$topics = array();
+
+		// Find all of the old MOVE topic notices that were set to expire
+		$request = $smcFunc['db_query']('', '
+			SELECT id_topic
+			FROM {db_prefix}topics
+			WHERE redirect_expires <= {int:redirect_expires}
+				AND redirect_expires <> 0',
+			array(
+				'redirect_expires' => time(),
+			)
+		);
+
+		while ($row = $smcFunc['db_fetch_row']($request))
+			$topics[] = $row[0];
+		$smcFunc['db_free_result']($request);
+
+		// Zap, your gone
+		if (count($topics) > 0)
+		{
+			require_once($sourcedir . '/RemoveTopic.php');
+			removeTopics($topics, false, true);
+		}
+
+		return true;
+	}
+}
--- other/install_3-0_mysql.sql
+++ other/install_3-0_mysql.sql
@@ -1775,7 +1775,7 @@ VALUES
   (9, 0, 0, 1, 'w', 0, 'weekly_maintenance', 'StoryBB\\Task\\Schedulable\\WeeklyMaintenance'),
   (10, 0, 120, 1, 'd', 1, 'paid_subscriptions', 'StoryBB\\Task\\Schedulable\\UpdatePaidSubs'),
   (11, 0, 120, 1, 'd', 0, 'remove_temp_attachments', 'StoryBB\\Task\\Schedulable\\RemoveTempAttachments'),
-  (12, 0, 180, 1, 'd', 0, 'remove_topic_redirect', ''),
+  (12, 0, 180, 1, 'd', 0, 'remove_topic_redirect', 'StoryBB\\Task\\Schedulable\\RemoveTopicRedirects'),
   (13, 0, 240, 1, 'd', 0, 'remove_old_drafts', 'StoryBB\\Task\\Schedulable\\RemoveOldDrafts'),
   (14, 0, 300, 1, 'd', 0, 'clean_exports', 'StoryBB\\Task\\Schedulable\\CleanExports'),
   (15, 0, 360, 1, 'd', 0, 'scrub_logs', 'StoryBB\\Task\\Schedulable\\ScrubLogs');
