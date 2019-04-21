9fc5bb437a0542550cf224773d55c65bf6c7c9e0
Arantor <sleeping@myperch.org>
2019-04-21 21:05:55 +0100

    Move the news updater scheduled task into its own class.

--- other/install_3-0_mysql.sql
+++ other/install_3-0_mysql.sql
@@ -514,7 +514,7 @@ VALUES
   (3, 0, 60, 1, 'd', 0, 'daily_maintenance', 'StoryBB\\Task\\Schedulable\\DailyMaintenance'),
   (5, 0, 0, 1, 'd', 0, 'daily_digest', ''),
   (6, 0, 0, 1, 'w', 0, 'weekly_digest', ''),
-  (7, 0, {$sched_task_offset}, 1, 'd', 0, 'fetchStoryBBfiles', ''),
+  (7, 0, {$sched_task_offset}, 1, 'd', 0, 'fetchStoryBBfiles', 'StoryBB\\Task\\Schedulable\\FetchStoryBBFiles'),
   (8, 0, 0, 1, 'd', 1, 'birthdayemails', ''),
   (9, 0, 0, 1, 'w', 0, 'weekly_maintenance', 'StoryBB\\Task\\Schedulable\\WeeklyMaintenance'),
   (10, 0, 120, 1, 'd', 1, 'paid_subscriptions', 'StoryBB\\Task\\Schedulable\\UpdatePaidSubs'),
