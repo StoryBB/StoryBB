3085893a8f5e1472a46a600e02f2ffd87dc8e821
Arantor <sleeping@myperch.org>
2019-05-26 20:02:32 +0100

    Fix PHPCS warnings.

--- Themes/default/scripts/manage_languages.js
+++ Themes/default/scripts/manage_languages.js
@@ -2,11 +2,11 @@ $('#add-multilang').on("click", function(e) {
 	e.preventDefault();
 	var txt = $('#multilang').data('remove');
 	var html = '<dt>' + 
-			   '<button class="button" role="button">' + txt + '</button>' +
-			   '<input name="entry_key[]" type="text" value="">' +
-			   '</dt>' +
-			   '<dd>' +
-			   '<textarea name="entry_value[]"></textarea>' +
+			   '<button class="button" role="button">' + txt + '</button>' + 
+			   '<input name="entry_key[]" type="text" value="">' + 
+			   '</dt>' + 
+			   '<dd>' + 
+			   '<textarea name="entry_value[]"></textarea>' + 
 			   '</dd>';
 	$('#multilang').append(html);
 	add_entry();
