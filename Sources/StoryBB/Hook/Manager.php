0f14570b1a7383674708efb95436033e25eae981
Arantor <sleeping@myperch.org>
2019-08-18 15:56:24 +0100

    Initial hook infrastructure.

---
+++ Sources/StoryBB/Hook/Manager.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This hook abstract forms the basis of a read-only hook.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\Hook;
+
+use InvalidArgumentException;
+use StoryBB\Hook\Hook;
+
+/**
+ * This hook abstract forms the basis of a read-only hook.
+ */
+class Manager
+{
+	protected static $hooks = [];
+
+	public static function register(string $hook, int $priority, $function)
+	{
+		static::$hooks[$hook][$priority][] = $function;
+		ksort(static::$hooks[$hook]);
+	}
+
+	public static function execute(Hookable $hook)
+	{
+		$classname = get_class($hook);
+		if (!isset(static::$hooks[$classname]))
+		{
+			return;
+		}
+
+		foreach (static::$hooks[$classname] as $priority => $priorityhooks)
+		{
+			foreach ($priorityhooks as $hookpoint)
+			{
+				if (is_callable($hookpoint))
+				{
+					call_user_func($hookpoint, $hook);
+				}
+			}
+		}
+	}
+}
---
+++ Sources/StoryBB/Hook/Observable/PostCreated.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This hook runs when a post is created.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\Hook\Observable;
+
+/**
+ * This hook runs when a post is created.
+ */
+class PostCreated extends \StoryBB\Hook\Observable
+{
+	protected $vars = [];
+
+	public function __construct($msgOptions, $topicOptions, $posterOptions, $message_columns, $message_parameters)
+	{
+		$this->vars = [
+			'msgOptions' => $msgOptions,
+			'topicOptions' => $topicOptions,
+			'posterOptions' => $posterOptions,
+			'message_columns' => $message_columns,
+			'message_parameters' => $message_parameters,
+		];
+	}
+}
