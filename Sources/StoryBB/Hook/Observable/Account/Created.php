229839c50dd2f51f581fea24b448a7eb5bfef648
Arantor <sleeping@myperch.org>
2019-09-23 16:08:24 +0100

    Add observable hook for account creation.

---
+++ Sources/StoryBB/Hook/Observable/Account/Created.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This hook runs when an account is created.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2018 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\Hook\Observable\Account;
+
+/**
+ * This hook runs when an account is created.
+ */
+class Created extends \StoryBB\Hook\Observable
+{
+	protected $vars = [];
+
+	public function __construct($memberID, array $regOptions)
+	{
+		$this->vars = [
+			'memberID' => $memberID,
+			'regOptions' => $regOptions,
+		];
+	}
+}
