ff5d855b9f2a70beb308c79c82d7b6b97bfb3b6e
Arantor <sleeping@myperch.org>
2020-03-28 16:45:36 +0000

    A route for serving smileys in the new filesystem.

---
+++ Sources/StoryBB/FileHandler/Servable.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Any file handlers must implement this interface.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2020 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\FileHandler;
+
+use StoryBB\Discoverable;
+
+/**
+ * Any file handlers must implement this interface.
+ */
+interface Servable extends Discoverable
+{
+
+}
