eceacd97303a62158fd7b4ba71265b33199e3e3a
Arantor <sleeping@myperch.org>
2020-04-25 12:02:32 +0100

    Remove legacy (and broken) login token check.

--- Sources/Security.php
+++ Sources/Security.php
@@ -748,18 +748,6 @@ function validateToken($action, $type = 'post', $reset = true)
 {
 	$type = $type == 'get' || $type == 'request' ? $type : 'post';
 
-	// Logins are special: the token is used to has the password with javascript before POST it
-	if ($action == 'login')
-	{
-		if (isset($_SESSION['token'][$type . '-' . $action]))
-		{
-			$return = $_SESSION['token'][$type . '-' . $action][3];
-			unset($_SESSION['token'][$type . '-' . $action]);
-			return $return;
-		}
-		else
-			return '';
-	}
 	$container = Container::instance();
 	$session = $container->get('session');
 
