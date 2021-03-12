17bf502893b4f9348f9ce72bfea08e75da53af02
Arantor <sleeping@myperch.org>
2021-03-12 22:27:24 +0000

    Add another couple of actions whose presence should not be logged for online purposes (#992)

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ function sbb_main()
 	if (!empty($topic) && empty($board_info['cur_topic_approved']) && !allowedTo('approve_posts') && ($user_info['id'] != $board_info['cur_topic_starter'] || $user_info['is_guest']))
 		fatal_lang_error('not_a_topic', false);
 
-	$no_stat_actions = ['autocomplete', 'dlattach', 'jsoption', 'likes', 'suggest', '.xml', 'xmlhttp', 'verificationcode', 'viewquery'];
+	$no_stat_actions = ['autocomplete', 'dlattach', 'jsoption', 'likes', 'suggest', '.xml', 'xmlhttp', 'verificationcode', 'viewquery', 'reagreement', 'reattributepost'];
 	call_integration_hook('integrate_pre_log_stats', [&$no_stat_actions]);
 	// Do some logging, unless this is an attachment, avatar, toggle of editor buttons, theme option, XML feed etc.
 	if (!in_array($context['current_action'], $no_stat_actions))
