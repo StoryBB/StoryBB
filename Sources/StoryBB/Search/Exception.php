5ffe88d19e4746c64178822adff8b4973890c338
Arantor <sleeping@myperch.org>
2021-05-16 19:34:25 +0100

    Add some search exceptions for use later.

---
+++ Sources/StoryBB/Search/Exception.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Search related excceptions.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2021 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\Search;
+
+/**
+ * Exception for all database queries.
+ */
+class Exception extends \StoryBB\Exception
+{
+}
---
+++ Sources/StoryBB/Search/Exception/InvalidAdapterException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Exceptions from within the search system.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2021 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\Search\Exception;
+
+/**
+ * Exceptions from within the database system.
+ */
+class InvalidAdapterException extends \StoryBB\Search\Exception
+{
+}
