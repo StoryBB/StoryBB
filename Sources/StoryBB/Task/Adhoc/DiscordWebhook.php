5ae5054507ab8828b5cec870cc7f7ed3d122d9b4
Arantor <sleeping@myperch.org>
2021-07-23 23:19:58 +0100

    Fix PHPCS warnings.

--- Sources/StoryBB/Task/Adhoc/DiscordWebhook.php
+++ Sources/StoryBB/Task/Adhoc/DiscordWebhook.php
@@ -19,26 +19,26 @@ use StoryBB\Integration\Discord;
  */
 class DiscordWebhook extends \StoryBB\Task\Adhoc
 {
-    use CharacterDetails;
+	use CharacterDetails;
 
-    public function execute()
-    {
-        $discord = new Discord();
+	public function execute()
+	{
+		$discord = new Discord();
 
-        // Get a fallback for posters.
-        $posted_by = $this->get_character_details(0, 0);
+		// Get a fallback for posters.
+		$posted_by = $this->get_character_details(0, 0);
 
-        if (isset($this->_details['msgid']))
-        {
-            $posted_by_ids = $this->get_post_owner($this->_details['msgid']);
-            if (isset($posted_by_ids['id_member'], $posted_by_ids['id_character']))
-            {
-                $posted_by = $this->get_character_details((int) $posted_by_ids['id_member'], (int) $posted_by_ids['id_character']);
-            }
-        }
+		if (isset($this->_details['msgid']))
+		{
+			$posted_by_ids = $this->get_post_owner($this->_details['msgid']);
+			if (isset($posted_by_ids['id_member'], $posted_by_ids['id_character']))
+			{
+				$posted_by = $this->get_character_details((int) $posted_by_ids['id_member'], (int) $posted_by_ids['id_character']);
+			}
+		}
 
-        $discord->send_webhook($this->_details['config']['webhook_url'], $this->_details['message'], $posted_by['username'], $posted_by['avatar'], $this->_details['embeds']);
+		$discord->send_webhook($this->_details['config']['webhook_url'], $this->_details['message'], $posted_by['username'], $posted_by['avatar'], $this->_details['embeds']);
 
-        return true;
-    }
+		return true;
+	}
 }
