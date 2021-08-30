4b8ff0811c08cd402a01bd8f56967df40efc1617
Arantor <sleeping@myperch.org>
2021-08-30 12:11:03 +0100

    Copy theme should copy the important files and fix refs. (#1070)

--- Sources/Themes.php
+++ Sources/Themes.php
@@ -1314,13 +1314,20 @@ function InstallCopy()
 	mkdir($context['to_install']['theme_dir'] . '/scripts', 0777);
 
 	// Copy over the default non-theme files.
-	$to_copy = ['/index.php', '/css/index.css', '/css/adaptive.css', '/css/rtl.css', '/css/admin.css', '/scripts/theme.js'];
+	$to_copy = ['/index.php', '/css/_variables.scss', '/css/index.scss', '/css/adaptive.css', '/css/rtl.css', '/css/admin.css', '/scripts/theme.js'];
 
 	foreach ($to_copy as $file)
 	{
 		copy($settings['default_theme_dir'] . $file, $context['to_install']['theme_dir'] . $file);
 		sbb_chmod($context['to_install']['theme_dir'] . $file, 0777);
 	}
+
+	// Now inject the new variables into the theme base.
+	$index_scss = file_get_contents($context['to_install']['theme_dir'] . '/css/index.scss');
+	$current_include = '@import "' . basename($settings['default_theme_dir']) . '/_variables.scss";';
+	$new_include = '@import "' . $context['to_install']['shortname'] . '/_variables.scss";';
+	$index_scss = str_replace($current_include, $current_include . "\n" . $new_include, $index_scss);
+	file_put_contents($context['to_install']['theme_dir'] . '/css/index.scss', $index_scss);
 
 	// And now the entire images directory!
 	copytree($settings['default_theme_dir'] . '/images', $context['to_install']['theme_dir'] . '/images');
