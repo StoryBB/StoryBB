12371c3d794fc77333c38493790c97553433fedc
Arantor <sleeping@myperch.org>
2021-10-30 23:52:30 +0100

    Exception I forgot to commit before.

---
+++ Sources/StoryBB/Routing/Exception/SessionTimeoutException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This is an exception relating to sessions in some fashion.
+ *
+ * @package StoryBB (storybb.org) - A roleplayer's forum software
+ * @copyright 2021 StoryBB and individual contributors (see contributors.txt)
+ * @license 3-clause BSD (see accompanying LICENSE file)
+ *
+ * @version 1.0 Alpha 1
+ */
+
+namespace StoryBB\Routing\Exception;
+
+/**
+ * This is an exception relating to sessions in some fashion.
+ */
+class SessionTimeoutException extends \StoryBB\Routing\Exception
+{
+}
